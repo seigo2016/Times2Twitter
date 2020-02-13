5084838a8e8807d78228f80b8455d4a41995817b
seigo2016 <2003seigo@gmail.com>
2020-02-13 20:55:02 +0900

    fix bug

--- src/gen_timeline.py
+++ src/gen_timeline.py
@@ -8,11 +8,8 @@ from slackeventsapi import SlackEventAdapter
 current_dir = os.path.dirname(os.path.abspath(__file__))
 config = configparser.ConfigParser()
 config.read(current_dir+'/token.ini')
-usertoken = os.getenv('SlackUtoken')
-slack_token = os.getenv('SlackBtoken')
-# slack_token = config.get("token", 'SlackBtoken')
-# usertoken = config.get("token", 'SlackUtoken')
-webclient = WebClient(token=usertoken)
+slack_token = config.get("token", 'SlackBtoken')
+web_client = WebClient(token=slack_token)
 rtm_client = RTMClient(token=slack_token)
 def get_chlist():
     chlist = []
@@ -35,60 +32,67 @@ def build_message(username, text, imgurlpub, msgchname, msgdate):
 
 @RTMClient.run_on(event="message")
 def get_msg(**payload):
-    ch, chinfo = get_chlist()
-    data = payload['data']
-    web_client = payload['web_client']
-    msgch = data['channel']
-    if 'user' in data and 'thread_ts' not in data:
-        user = data['user']
-        text = data['text']
-        if chinfo[msgch] == user and msgch in ch:
-            username = web_client.users_info(
-                token=slack_token,
-                user=user)['user']['profile']['display_name']
-            iconurl = web_client.users_info(
-                token=slack_token,
-                user=user)['user']['profile']['image_512']
-            msgchname = web_client.channels_info(token=slack_token, channel=msgch)[
-                'channel']['name']
-            msgdate = datetime.date.fromtimestamp(int(float(data['ts'])))
-            block = build_message(
-                username, text, iconurl, msgchname, msgdate)
-            web_client.chat_postMessage(
-                token=os.getenv('SlackBtoken'),
-                channel="#times_all_tl",
-                unfurl_media=True,
-                unfurl_links=True,
-                blocks=block
-            )
-    # except Exception as e:
-    #     print(e)
-    #     pass
+    try:
+        ch, chinfo = get_chlist()
+        data = payload['data']
+        web_client = payload['web_client']
+        msgch = data['channel']
+        if 'user' in data and 'thread_ts' not in data:
+            user = data['user']
+            text = data['text']
+            if chinfo[msgch] == user and msgch in ch:
+                username = web_client.users_info(
+                    token=slack_token,
+                    user=user)['user']['profile']['display_name']
+                iconurl = web_client.users_info(
+                    token=slack_token,
+                    user=user)['user']['profile']['image_512']
+                msgchname = web_client.channels_info(token=slack_token, channel=msgch)[
+                    'channel']['name']
+                msgdate = datetime.date.fromtimestamp(int(float(data['ts'])))
+                block = build_message(
+                    username, text, iconurl, msgchname, msgdate)
+                web_client.chat_postMessage(
+                    token=slack_token,
+                    channel="#times_all_tl",
+                    unfurl_media=True,
+                    unfurl_links=True,
+                    blocks=block
+                )
+    except Exception as e:
+        print(e)
 
 def channel_list(client):
-    channels = client.api_call("channels.list")
+    channels = client.channels_list(token=slack_token)
+    print(channels)
     if channels['ok']:
         return channels['channels']
+    else:
+        return None
+
 
 @RTMClient.run_on(event="channel_joined")
 def app_invited(**payload):
     chlist = []
-    ret = channel_list(webclient)
+    ret = channel_list(web_client)
     for i in ret:
         if i['name'].find("times_all_tl") is not -1:
             tlch = i
         elif i['name'].find("times") is not -1:
             chlist.append(i)
-    with open('channel.csv', 'w') as f:
-        writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created',
-                                    'is_archived', 'is_general', 'unlinked', 'creator', 'name_normalized', 'is_shared', 'is_org_shared', 'is_member', 'is_private', 'is_mpim', 'members', 'topic', 'purpose', 'previous_names', 'num_members'])
+    with open(current_dir + '/channel.csv', 'w') as f:
+        writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created', 'creator', "is_archived", "is_general", "name_normalized", "is_shared",
+        "is_org_shared", "is_member", "is_private", "is_mpim", "members", "topic", "purpose", "previous_names", "num_members", "unlinked",
+        "pending_connected_team_ids", "is_ext_shared", "is_group", "pending_shared", "is_pending_ext_shared", "shared_team_ids", "parent_conversation", "is_im"])
         writer.writeheader()
         for i in chlist:
             writer.writerow(i)
-    with open('tl.csv', 'w') as f:
-        writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created',
-                                    'is_archived', 'is_general', 'unlinked', 'creator', 'name_normalized', 'is_shared', 'is_org_shared', 'is_member', 'is_private', 'is_mpim', 'members', 'topic', 'purpose', 'previous_names', 'num_members'])
+    with open(current_dir + '/tl.csv', 'w') as f:
+        writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created', 'creator', "is_archived", "is_general", "name_normalized", "is_shared",
+        "is_org_shared", "is_member", "is_private", "is_mpim", "members", "topic", "purpose", "previous_names", "num_members", "unlinked",
+        "pending_connected_team_ids", "is_ext_shared", "is_group", "pending_shared", "is_pending_ext_shared", "shared_team_ids", "parent_conversation", "is_im"])
         writer.writeheader()
         writer.writerow(tlch)
+
 
 rtm_client.start()
--- src/get_times.py
+++ src/get_times.py
@@ -5,17 +5,15 @@ import configparser
 current_dir = os.path.dirname(os.path.abspath(__file__))
 config = configparser.ConfigParser()
 config.read(current_dir+'/token.ini')
-usertoken = os.getenv('SlackUtoken')
-slack_token = os.getenv('SlackBtoken')
-# slack_token = config.get("token", 'SlackBtoken')
-# usertoken = config.get("token", 'SlackUtoken')
-web_client = WebClient(token=usertoken)
+slack_token = config.get("token", 'SlackBtoken')
+web_client = WebClient(token=slack_token)
 rtm_client = RTMClient(token=slack_token)
 chlist = []
 
 
 def channel_list(client):
-    channels = client.api_call("channels.list")
+    channels = client.channels_list(token=slack_token)
+    print(channels)
     if channels['ok']:
         return channels['channels']
     else:
@@ -30,13 +28,15 @@ for i in ret:
         chlist.append(i)
 
 with open(current_dir + '/channel.csv', 'w') as f:
-    writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created',
-                                'is_archived', 'is_general', 'unlinked', 'creator', 'name_normalized', 'is_shared', 'is_org_shared', 'is_member', 'is_private', 'is_mpim', 'members', 'topic', 'purpose', 'previous_names', 'num_members'])
+    writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created', 'creator', "is_archived", "is_general", "name_normalized", "is_shared",
+    "is_org_shared", "is_member", "is_private", "is_mpim", "members", "topic", "purpose", "previous_names", "num_members", "unlinked",
+    "pending_connected_team_ids", "is_ext_shared", "is_group", "pending_shared", "is_pending_ext_shared", "shared_team_ids", "parent_conversation", "is_im"])
     writer.writeheader()
     for i in chlist:
         writer.writerow(i)
 with open(current_dir + '/tl.csv', 'w') as f:
-    writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created',
-                                'is_archived', 'is_general', 'unlinked', 'creator', 'name_normalized', 'is_shared', 'is_org_shared', 'is_member', 'is_private', 'is_mpim', 'members', 'topic', 'purpose', 'previous_names', 'num_members'])
+    writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created', 'creator', "is_archived", "is_general", "name_normalized", "is_shared",
+    "is_org_shared", "is_member", "is_private", "is_mpim", "members", "topic", "purpose", "previous_names", "num_members", "unlinked",
+    "pending_connected_team_ids", "is_ext_shared", "is_group", "pending_shared", "is_pending_ext_shared", "shared_team_ids", "parent_conversation", "is_im"])
     writer.writeheader()
     writer.writerow(tlch)
