12641da03ab11f3842c7d15e16862422da6b972d
seigo2016 <2003seigo@gmail.com>
2020-02-13 13:11:37 +0900

    add reply check

--- src/gen_timeline.py
+++ src/gen_timeline.py
@@ -2,18 +2,22 @@ from slack import RTMClient, WebClient
 import csv
 import os
 import datetime
+import configparser
+from slackeventsapi import SlackEventAdapter
 
-
+current_dir = os.path.dirname(os.path.abspath(__file__))
+config = configparser.ConfigParser()
+config.read(current_dir+'/token.ini')
 usertoken = os.getenv('SlackUtoken')
 slack_token = os.getenv('SlackBtoken')
+# slack_token = config.get("token", 'SlackBtoken')
+# usertoken = config.get("token", 'SlackUtoken')
 webclient = WebClient(token=usertoken)
 rtm_client = RTMClient(token=slack_token)
-
-
 def get_chlist():
     chlist = []
     chinfo = {}
-    with open("channel.csv", "r") as f:
+    with open(current_dir+"/channel.csv", "r") as f:
         reader = csv.DictReader(f)
         for row in reader:
             chlist.append(row['id'])
@@ -35,9 +39,9 @@ def get_msg(**payload):
     data = payload['data']
     web_client = payload['web_client']
     msgch = data['channel']
-    if 'user' in data:
-        text = data['text']
+    if 'user' in data and 'thread_ts' not in data:
         user = data['user']
+        text = data['text']
         if chinfo[msgch] == user and msgch in ch:
             username = web_client.users_info(
                 token=slack_token,
@@ -48,18 +52,8 @@ def get_msg(**payload):
             msgchname = web_client.channels_info(token=slack_token, channel=msgch)[
                 'channel']['name']
             msgdate = datetime.date.fromtimestamp(int(float(data['ts'])))
-            if 'files' in data:
-                imgurl = webclient.files_sharedPublicURL(
-                    token=usertoken,
-                    file=data['files'][0]['id'])
-                imgsec = imgurl['file']['permalink_public'].split('-')[3]
-                imgurlpub = imgurl['file']['url_private'] + \
-                    "?pub_secret=" + imgsec
-                block = build_message(
-                    username, text, iconurl, msgchname, msgdate)
-            else:
-                block = build_message(
-                    username, text, iconurl, msgchname, msgdate)
+            block = build_message(
+                username, text, iconurl, msgchname, msgdate)
             web_client.chat_postMessage(
                 token=os.getenv('SlackBtoken'),
                 channel="#times_all_tl",
@@ -67,6 +61,34 @@ def get_msg(**payload):
                 unfurl_links=True,
                 blocks=block
             )
+    # except Exception as e:
+    #     print(e)
+    #     pass
 
+def channel_list(client):
+    channels = client.api_call("channels.list")
+    if channels['ok']:
+        return channels['channels']
+
+@RTMClient.run_on(event="channel_joined")
+def app_invited(**payload):
+    chlist = []
+    ret = channel_list(webclient)
+    for i in ret:
+        if i['name'].find("times_all_tl") is not -1:
+            tlch = i
+        elif i['name'].find("times") is not -1:
+            chlist.append(i)
+    with open('channel.csv', 'w') as f:
+        writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created',
+                                    'is_archived', 'is_general', 'unlinked', 'creator', 'name_normalized', 'is_shared', 'is_org_shared', 'is_member', 'is_private', 'is_mpim', 'members', 'topic', 'purpose', 'previous_names', 'num_members'])
+        writer.writeheader()
+        for i in chlist:
+            writer.writerow(i)
+    with open('tl.csv', 'w') as f:
+        writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created',
+                                    'is_archived', 'is_general', 'unlinked', 'creator', 'name_normalized', 'is_shared', 'is_org_shared', 'is_member', 'is_private', 'is_mpim', 'members', 'topic', 'purpose', 'previous_names', 'num_members'])
+        writer.writeheader()
+        writer.writerow(tlch)
 
 rtm_client.start()
--- src/get_times.py
+++ src/get_times.py
@@ -1,10 +1,16 @@
-import slack
+from slack import WebClient, RTMClient
 import csv
 import os
-usertoken = os.getenv('SlackBtoken')
-client = slack.WebClient(
-    token=usertoken)
-
+import configparser
+current_dir = os.path.dirname(os.path.abspath(__file__))
+config = configparser.ConfigParser()
+config.read(current_dir+'/token.ini')
+usertoken = os.getenv('SlackUtoken')
+slack_token = os.getenv('SlackBtoken')
+# slack_token = config.get("token", 'SlackBtoken')
+# usertoken = config.get("token", 'SlackUtoken')
+web_client = WebClient(token=usertoken)
+rtm_client = RTMClient(token=slack_token)
 chlist = []
 
 
@@ -16,20 +22,20 @@ def channel_list(client):
         return None
 
 
-ret = channel_list(client)
+ret = channel_list(web_client)
 for i in ret:
     if i['name'].find("times_all_tl") is not -1:
         tlch = i
     elif i['name'].find("times") is not -1:
         chlist.append(i)
 
-with open('channel.csv', 'w') as f:
+with open(current_dir + '/channel.csv', 'w') as f:
     writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created',
                                 'is_archived', 'is_general', 'unlinked', 'creator', 'name_normalized', 'is_shared', 'is_org_shared', 'is_member', 'is_private', 'is_mpim', 'members', 'topic', 'purpose', 'previous_names', 'num_members'])
     writer.writeheader()
     for i in chlist:
         writer.writerow(i)
-with open('tl.csv', 'w') as f:
+with open(current_dir + '/tl.csv', 'w') as f:
     writer = csv.DictWriter(f, ['id', 'name', 'is_channel', 'created',
                                 'is_archived', 'is_general', 'unlinked', 'creator', 'name_normalized', 'is_shared', 'is_org_shared', 'is_member', 'is_private', 'is_mpim', 'members', 'topic', 'purpose', 'previous_names', 'num_members'])
     writer.writeheader()
